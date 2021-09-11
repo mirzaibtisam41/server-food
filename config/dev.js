8762820cb09f11280859f5b9769029da31903234
mirzaibtisam41 <mirzaibtisam41@gmail.com>
2021-09-11 23:06:46 +0500

    new

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,4 @@
 module.exports = {
-    "mongoURI": "mongodb://localhost:27017/FoodApp",
-    // "mongoURI": "mongodb+srv://subhan:pCIwoawsFJPlxRaW@cluster0.5ktxj.mongodb.net/foodApp?retryWrites=true&w=majority?ssl=true",
+    "mongoURI": "mongodb+srv://subhan:pCIwoawsFJPlxRaW@cluster0.5ktxj.mongodb.net/foodApp?retryWrites=true&w=majority?ssl=true",
     "jwtSecret": "mySecretToken"
 }
