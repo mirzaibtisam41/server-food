f8ae8db4890369064ebbb9a2aefcd07d9affb1ed
mirzaibtisam41 <mirzaibtisam41@gmail.com>
2021-10-02 17:15:30 +0500

    stripe

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const DB = require("./config/db");
 const Emitter = require('events');
 const eventEmitter = new Emitter();
+const stripe = require('stripe');
 
 // middlewares
 app.use("/uploads", express.static("uploads"));
@@ -26,6 +27,26 @@ app.use("/api/vendor", require("./routes/vendorRoute"));
 app.use("/api/product", require("./routes/productRoute"));
 app.use("/api/order", require("./routes/orderRoute"));
 
+// stripe payment
+app.post('/api/payment', async (req, res) => {
+    // Use an existing Customer ID if this is a returning customer.
+    const customer = await stripe.customers.create();
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+        { customer: customer.id },
+        { apiVersion: '2020-08-27' }
+    );
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: 1099,
+        currency: 'usd',
+        customer: customer.id,
+    });
+    res.json({
+        paymentIntent: paymentIntent.client_secret,
+        ephemeralKey: ephemeralKey.secret,
+        customer: customer.id
+    });
+});
+
 // server listen
 const PORT = process.env.PORT || 8000;
 const server = app.listen(PORT, console.log('Server listening on port ' + PORT));
@@ -41,4 +62,4 @@ io.on('connection', socket => {
         io.to(senderID).emit('getMessage', message);
         io.to(receiverID).emit('getMessage', message);
     });
-})
+});
