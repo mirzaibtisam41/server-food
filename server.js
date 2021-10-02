7222769e7370c967bae34e148c6e3f797ab8f0b2
mirzaibtisam41 <mirzaibtisam41@gmail.com>
2021-10-02 18:09:39 +0500

    stripe done

--- server.js
+++ server.js
@@ -28,24 +28,24 @@ app.use("/api/product", require("./routes/productRoute"));
 app.use("/api/order", require("./routes/orderRoute"));
 
 // stripe payment
-// app.post('/api/payment', async (req, res) => {
-//     // Use an existing Customer ID if this is a returning customer.
-//     const customer = await stripe.customers.create();
-//     const ephemeralKey = await stripe.ephemeralKeys.create(
-//         { customer: customer.id },
-//         { apiVersion: '2020-08-27' }
-//     );
-//     const paymentIntent = await stripe.paymentIntents.create({
-//         amount: 1099,
-//         currency: 'usd',
-//         customer: customer.id,
-//     });
-//     res.json({
-//         paymentIntent: paymentIntent.client_secret,
-//         ephemeralKey: ephemeralKey.secret,
-//         customer: customer.id
-//     });
-// });
+app.post('/api/payment', async (req, res) => {
+    // Use an existing Customer ID if this is a returning customer.
+    const customer = await stripe.customers.create();
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+        { customer: customer.id },
+        { apiVersion: '2020-08-27' }
+    );
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: 1099,
+        currency: 'usd',
+        customer: customer.id,
+    });
+    res.json({
+        paymentIntent: paymentIntent.client_secret,
+        ephemeralKey: ephemeralKey.secret,
+        customer: customer.id
+    });
+});
 
 // server listen
 const PORT = process.env.PORT || 8000;
